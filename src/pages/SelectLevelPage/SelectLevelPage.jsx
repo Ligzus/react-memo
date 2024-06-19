9c2204a8a0566d85b00143ae8e17e13e1856db6c
Ligzus2 <infopop@mail.ru>
2024-06-19 16:08:26 +0300

    Добавлен и стилизован чекбокс легкого режима

--- src/pages/SelectLevelPage/SelectLevelPage.jsx
+++ src/pages/SelectLevelPage/SelectLevelPage.jsx
@@ -1,28 +1,43 @@
-import { Link } from "react-router-dom";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import styles from "./SelectLevelPage.module.css";
 
 export function SelectLevelPage() {
+  const [isEasyMode, setIsEasyMode] = useState(false);
+  const navigate = useNavigate();
+
+  const handleLevelClick = pairsCount => {
+    navigate(`/game/${pairsCount}?easyMode=${isEasyMode}`);
+  };
+
   return (
     <div className={styles.container}>
       <div className={styles.modal}>
         <h1 className={styles.title}>Выбери сложность</h1>
         <ul className={styles.levels}>
           <li className={styles.level}>
-            <Link className={styles.levelLink} to="/game/3">
+            <button className={styles.levelLink} onClick={() => handleLevelClick(3)}>
               1
-            </Link>
+            </button>
           </li>
           <li className={styles.level}>
-            <Link className={styles.levelLink} to="/game/6">
+            <button className={styles.levelLink} onClick={() => handleLevelClick(6)}>
               2
-            </Link>
+            </button>
           </li>
           <li className={styles.level}>
-            <Link className={styles.levelLink} to="/game/9">
+            <button className={styles.levelLink} onClick={() => handleLevelClick(9)}>
               3
-            </Link>
+            </button>
           </li>
         </ul>
+        <div className={styles.easyModeToggle}>
+          <label className={styles.checkboxContainer}>
+            <input type="checkbox" checked={isEasyMode} onChange={e => setIsEasyMode(e.target.checked)} />
+            Легкий режим
+            <span className={styles.checkmark}></span>
+          </label>
+        </div>
       </div>
     </div>
   );
